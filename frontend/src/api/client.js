d26a996c800b9c0e850bd81da2e1414faef409c2
Sayan <sayantanman508@gmail.com>
2026-09-13 04:04:34 +0530

    fix: update EC2 IP to elastic IP 13.206.64.164

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const client = axios.create({
   // production EC2 backend
-  baseURL: 'http://13.201.192.30:8000',
+  baseURL: 'http://13.206.64.164:8000',
   headers: {
     'Content-Type': 'application/json',
   },
